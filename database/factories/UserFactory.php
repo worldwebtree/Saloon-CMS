ef78ca466cf58cb0fa8f40b64952c39c917033de
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-10-22 16:09:08 +0500

    Modified database seeder with an example admin credentials also edited the user factory with new fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,10 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'contact-no' => fake()->phoneNumber(),
+            'gender' => fake()->randomElement(['male', 'female', 'custom']),
+            'role' => fake()->randomElement(['super-admin', 'admin', 'customer']),
+            'avatar' => 'example-image.png',
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +18,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'SuperAdmin@example.com',
+            'contact-no' => '03319218866',
+            'gender' => 'male',
+            'role' => 'super-admin',
+            'avatar' => 'example-image.png',
+        ]);
     }
 }
